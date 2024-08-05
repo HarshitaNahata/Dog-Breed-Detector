668dc95117853c6e7a88c56f705b2a419ebefaf9
Harshita Nahata <khushinahata04@gmail.com>
2024-08-05 21:28:17 +0530

    all files added

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:camera/camera.dart';
+import 'package:dog_breed/home.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+List<CameraDescription>? cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Dog Breed Detector',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
